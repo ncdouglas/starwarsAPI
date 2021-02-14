21be8905636c6f2889d8bfc0b5cdaa6ee85ea637
Natasha Douglas <ncdouglas2016@gmail.com>
2021-02-14 11:51:50 -0600

    commit following tutorial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Container, Dimmer, Loader } from 'semantic-ui-react';
+import Home from './components/Home';
+import People from './components/People';
+import Planets from './components/Planets';
 
 function App() {
+  const [people, setPeople] = useState([]);
+  const [planets, setPlanets] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchPeople() {
+
+      let res = await fetch('https://swapi.dev/api/people/?format=json');
+      let data = await res.json();
+      setPeople(data.results);
+      setLoading(false);
+    }
+
+    async function fetchPlanets() {
+      let res = await fetch('https://swapi.dev/api/planets/?format=json');
+      let data = await res.json();
+      setPlanets(data.results);
+      setLoading(false);
+    }
+
+    fetchPeople();
+    fetchPlanets();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Container>
+          {loading ? (
+            <Dimmer active inverted>
+              <Loader inverted>Loading</Loader>
+            </Dimmer>
+          ) : (
+            <Switch>
+              <Route exact path='/'>
+                <Home />
+              </Route>
+              <Route exact path='/people'>
+                <People data={people}/>
+              </Route>
+              <Route exact path='/planets'>
+                <Planets data={planets}/>
+              </Route>
+          </Switch>
+          )}
+        </Container>
+      </Router>
+      
+    </>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div>
+            <p>Welcome to my react project using the Star Wars API, react-router-dom, semantic-ui-react, and semantic-ui-css. 
+            This project is based on a project presented by Jason Rivera here: https://www.youtube.com/watch?v=EC5ZvP87P2k.
+            </p>
+        </div>
+    )
+}
